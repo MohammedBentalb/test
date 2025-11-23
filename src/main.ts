342276a072593c1aceb690e9d68d781175819e37
Mohammed Bentalb <mohammedbentalb0@gmail.com>
2025-11-23 17:16:34 +0100

    aws: fix step name

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,95 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const form = document.querySelector("form")!;
+const nameInput = document.querySelector<HTMLInputElement>("#name-inp")!;
+const selectInput = document.querySelector<HTMLSelectElement>("#role")!;
+const nameErrorField =
+  document.querySelector<HTMLParagraphElement>(".error-name")!;
+const roleErrorField =
+  document.querySelector<HTMLParagraphElement>(".error-role")!;
+const List = document.querySelector<HTMLUListElement>(".list")!;
+const modal = document.querySelector<HTMLDivElement>(".modal")!;
+const addButton = document.querySelector<HTMLButtonElement>(".add")!;
+const cancelButton = document.querySelector<HTMLButtonElement>("#cancel")!;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let empList: { name: string; role: string }[] | [] = [];
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const validateInput = (input: HTMLInputElement | HTMLSelectElement) => {
+  const value = input.value.trim();
+  if (!value) {
+    // console.log("innvalid Value");
+    return "invalid Value";
+  }
+  console.log(input.value);
+  return "";
+};
+
+const showError = (element: HTMLParagraphElement, err: string) => {
+  element.textContent = err;
+};
+
+// validate the user name choice
+nameInput.addEventListener("input", () => {
+  console.log("hi there");
+  const errMessage = validateInput(nameInput);
+  showError(nameErrorField as HTMLInputElement, errMessage);
+});
+
+// validate the user role
+selectInput.addEventListener("change", () => {
+  console.log("hi there");
+  const errMessage = validateInput(selectInput);
+  showError(roleErrorField, errMessage);
+});
+
+// handle the si=ubmit action
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const eName = validateInput(nameInput);
+  const eRole = validateInput(selectInput);
+
+  showError(nameErrorField, eName);
+  showError(roleErrorField, eRole);
+
+  if (!eName && !eRole) {
+    empList = [
+      ...empList,
+      { name: nameInput.value.trim(), role: selectInput.value.trim() },
+    ];
+    console.log(selectInput.value.trim());
+
+    renderEmployees();
+    modal.classList.add("is-hidden");
+
+    form.reset();
+    console.log(modal);
+    console.log(empList);
+  } else {
+    console.log("bruuuh");
+  }
+});
+
+// totgle the modal to be visible
+addButton.addEventListener("click", () => {
+  modal.classList.remove("is-hidden");
+});
+
+// totgle the modal to be not be visible
+cancelButton.addEventListener("click", () => {
+  modal.classList.add("is-hidden");
+});
+
+function renderEmployees() {
+  List.innerHTML = "";
+  empList.forEach((emp) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <div class="img"></div>
+          <div>
+            <p>${emp.name}</p>
+            <p>${emp.role}</p>
+        </div>
+        <button>Edit</button>
+        `;
+    List.appendChild(li);
+  });
+}
